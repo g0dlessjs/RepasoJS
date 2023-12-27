bd8016323bd5469b422fe2299c43e5d9feb63098
g0dlessjs <juangalarce.dev@gmail.com>
2023-12-27 08:16:55 -0300

    Manipulacion de 2 o mas objetos

--- js/06.js
+++ js/06.js
@@ -1,1 +1,21 @@
 // Manipulacio de 2 o mas Objetos
+
+const producto = {
+  nombre: "Mouse",
+  precio: 20000,
+  disponibilidad: true,
+};
+
+const cliente = {
+  nombre: "Corey",
+  premium: true,
+};
+
+// Esto no se puede hacer porque tendriamos la variable nombre repetida,
+// La solucion es darle un alias, asi no usaremos el mismo nombre
+
+const { nombre, precio, disponibilidad } = producto;
+const { nombre: nombreCliente, premium } = cliente;
+
+console.log(nombre, precio, disponibilidad);
+console.log(nombreCliente, premium);
